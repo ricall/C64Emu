96b520d9e01adc1629223a1442cf0f1df6b48529
DanielLeC64 <dschult@gmx.de>
2018-12-15 22:17:52 +0100

    added ROL, LSR

--- src/c64/emulation/instructionset/Shift.kt
+++ src/c64/emulation/instructionset/Shift.kt
@@ -15,7 +15,9 @@ class Shift(private var cpu: CPU, private var registers: Registers, @Suppress("u
     init {
         cpu.registerInstruction(0x06, ::opASL)
         cpu.registerInstruction(0x0A, ::opASL)
+        cpu.registerInstruction(0x26, ::opROL)
         cpu.registerInstruction(0x2A, ::opROL)
+        cpu.registerInstruction(0x46, ::opLSR)
         cpu.registerInstruction(0x4A, ::opLSR)
         cpu.registerInstruction(0x6A, ::opROR)
     }
@@ -25,13 +27,31 @@ class Shift(private var cpu: CPU, private var registers: Registers, @Suppress("u
      */
     private fun opROL() {
         // todo: switch for 5 addressing modes...
+        var result = 0
         when (cpu.currentOpcode.toInt()) {
+            0x26 -> {
+                // addressing mode: zeropage
+                // cycles: 5
+                registers.cycles += 5
+                // get zeropage address
+                val addr = memory.fetchZeroPageAddressWithPC()
+                // shift left by 1
+                result = memory.fetch(addr).toInt() shl 1
+                // fill bit 0 with the value of the carry flag
+                if (registers.C) {
+                    result = result or 0x1
+                }
+                // save bit 8 in the carry flag...
+                registers.C = result and 0x100 == 0x100
+                // push value back to zeropage address
+                memory.push(addr, result.toUByte())
+            }
             0x2A -> {
                 // addressing mode: accumulator
                 // cycles: 2
                 registers.cycles += 2
                 // shift left by 1
-                var result = registers.A.toInt() shl 1
+                result = registers.A.toInt() shl 1
                 // fill bit 0 with the value of the carry flag
                 if (registers.C) {
                     result = result or 0x1
@@ -41,8 +61,8 @@ class Shift(private var cpu: CPU, private var registers: Registers, @Suppress("u
                 registers.A = result.toUByte()
             }
         }
-        registers.setZeroFlagFromValue(registers.A)
-        registers.setNegativeFlagFromValue(registers.A)
+        registers.setZeroFlagFromValue(result.toUByte())
+        registers.setNegativeFlagFromValue(result.toUByte())
     }
 
     /**
@@ -111,7 +131,22 @@ class Shift(private var cpu: CPU, private var registers: Registers, @Suppress("u
      */
     private fun opLSR() {
         // todo: switch for 5 addressing modes...
+        var result: UByte = 0u
         when (cpu.currentOpcode.toInt()) {
+            0x46 -> {
+                // addressing mode: zeropage
+                // cycles: 5
+                registers.cycles += 5
+                // get zeropage address
+                val addr = memory.fetchZeroPageAddressWithPC()
+                // save current value from zeropage
+                result = memory.fetch(addr)
+                // save bit 0 in the carry flag...
+                registers.C = result.toInt() and 0x01 == 0x01
+                // shift right by 1
+                result = (result.toInt() shr 1).toUByte()
+                memory.push(addr, result)
+            }
             0x4A -> {
                 // addressing mode: accumulator
                 // cycles: 2
@@ -119,10 +154,11 @@ class Shift(private var cpu: CPU, private var registers: Registers, @Suppress("u
                 // save bit 0 in the carry flag...
                 registers.C = registers.A.toInt() and 0x01 == 0x01
                 // shift right by 1
-                registers.A = (registers.A.toInt() shr 1).toUByte()
+                result = (registers.A.toInt() shr 1).toUByte()
+                registers.A = result
             }
         }
-        registers.setZeroFlagFromValue(registers.A)
+        registers.setZeroFlagFromValue(result)
         // negative flag always 0 after this operation
         registers.N = false
     }
