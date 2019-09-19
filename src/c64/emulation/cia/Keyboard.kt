de0de5d77f1a25a0462e914aa0e23efd9eb48d4c
Daniel Schulte <Daniel.Schulte@tomtom.com>
2019-09-19 15:43:02 +0200

    * keyboard support

--- src/c64/emulation/cia/Keyboard.kt
+++ src/c64/emulation/cia/Keyboard.kt
@@ -10,14 +10,15 @@ import java.awt.event.KeyListener
  * @author Daniel Schulte 2017-2019
  */
 @ExperimentalUnsignedTypes
-class Keyboard: KeyListener {
+class Keyboard : KeyListener {
 
     companion object {
-        var keyboardMatrix = arrayOf(
+        var SHIFT_CODE = 0xFF
+        var keyboardTranslationMatrix = arrayOf(
             //         DEL   RET   C-RI  F7    F1    F3    F5    C-DO
-            intArrayOf(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00),
+            intArrayOf(0x00, 0x0A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00),
             //         3     W     A     4     Z     S     E     LSHI
-            intArrayOf(0x33, 0x57, 0x41, 0x34, 0x5A, 0x53, 0x45, 0x00),
+            intArrayOf(0x33, 0x57, 0x41, 0x34, 0x5A, 0x53, 0x45, 0xFF),
             //         5     R     D     6     C     F     T     X
             intArrayOf(0x35, 0x52, 0x44, 0x36, 0x43, 0x46, 0x54, 0x58),
             //         7     Y     G     8     B     H     U     V
@@ -29,15 +30,18 @@ class Keyboard: KeyListener {
             //         £     *     ;     HOME  RSHI  =     ^     /
             intArrayOf(0x9C, 0x2A, 0x3B, 0x00, 0x00, 0x3D, 0x5E, 0x2F),
             //         1     <     CTRL  2     SPACE C=    Q     RUN/STOP
-            intArrayOf(0x31, 0x3C, 0x00, 0x32, 0x20, 0x00, 0x51, 0x00))
+            intArrayOf(0x31, 0x3C, 0x00, 0x32, 0x20, 0x00, 0x51, 0x00)
+        )
+        var keyboardTranslation = hashMapOf(KeyEvent.VK_PLUS to 0x2B, KeyEvent.VK_0 to 0x3D)
+        // todo var keyboardShiftTranslation = hashMapOf(KeyEvent.VK_PLUS to 0x2B, KeyEvent.VK_0 to 0x3D)
     }
 
     private var shiftState: Int = -1
-    private var lastCharCode: Int = -1
+    private var lastKeyCode: Int = -1
 
     fun getDataPortB(dataPortA: UByte): UByte {
         var result = 0
-        if (lastCharCode > -1) {
+        if (lastKeyCode > -1) {
             var column = 0
             val columns = ArrayList<Int>()
             var dataPortAInt = dataPortA.toInt() xor 0xFF
@@ -51,8 +55,14 @@ class Keyboard: KeyListener {
             }
             // find all pressed keys
             for (col in columns) {
-                val row = keyboardMatrix[col]
-                val keyIndex = row.indexOf(lastCharCode)
+                val row = keyboardTranslationMatrix[col]
+                // find key code
+                var keyIndex = row.indexOf(lastKeyCode)
+                if (keyIndex > -1) {
+                    result = result or (1 shl keyIndex)
+                }
+                // find shift
+                keyIndex = row.indexOf(shiftState)
                 if (keyIndex > -1) {
                     result = result or (1 shl keyIndex)
                 }
@@ -63,21 +73,52 @@ class Keyboard: KeyListener {
 
     override fun keyPressed(e: KeyEvent?) {
         if (e != null) {
-            if (e.modifiersEx and InputEvent.SHIFT_DOWN_MASK == InputEvent.SHIFT_DOWN_MASK) {
-                shiftState = 1
+            shiftState = -1
+            lastKeyCode = e.keyCode
+            val shiftDown = e.modifiersEx and InputEvent.SHIFT_DOWN_MASK == InputEvent.SHIFT_DOWN_MASK
+            if (shiftDown) {
+                when (lastKeyCode) {
+                    in KeyEvent.VK_A..KeyEvent.VK_Z,
+                    in KeyEvent.VK_1..KeyEvent.VK_2,
+                    in KeyEvent.VK_4..KeyEvent.VK_6,
+                    in KeyEvent.VK_8..KeyEvent.VK_9 -> {
+                        // keys A-Z, 1-2, 4-6, 8-9
+                        // ==> normal shift handling
+                        shiftState = SHIFT_CODE
+                    }
+                    KeyEvent.VK_0 -> {
+                        lastKeyCode = keyboardTranslation[lastKeyCode]
+                    }
+                    KeyEvent.VK_7 -> {
+                        // shift+7 => /
+                        lastKeyCode = 0x2F
+                    }
+                    KeyEvent.VK_PERIOD -> {
+                        // shift+. => :
+                        lastKeyCode = 0x3A
+                    }
+                    KeyEvent.VK_COMMA -> {
+                        // shift+, => ;
+                        lastKeyCode = 0x3B
+                    }
+                }
             }
             else {
-                shiftState = 0
+                // todo: ? * # ' (^ should be left arrow) < > ESC (BACK shoudl be DEL) CURSOR-KEY
+                when (lastKeyCode) {
+                    KeyEvent.VK_PLUS -> {
+                        // translate +
+                        lastKeyCode = keyboardTranslation[lastKeyCode]
+                    }
+                }
             }
-            lastCharCode = e.keyChar.toInt()
-        }
-        else {
-            lastCharCode = -1
+        } else {
+            lastKeyCode = -1
         }
     }
 
     override fun keyReleased(e: KeyEvent?) {
-        lastCharCode = -1
+        lastKeyCode = -1
     }
 
     override fun keyTyped(e: KeyEvent?) {
