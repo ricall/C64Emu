d384b8b37c95e57dfdd62eaa4bc9e0d16658c39f
Daniel Schulte <Daniel.Schulte@tomtom.com>
2019-10-11 08:30:56 +0200

    * added multicolor bitmap rasterizer

--- src/c64/emulation/vic/VIC.kt
+++ src/c64/emulation/vic/VIC.kt
@@ -54,7 +54,9 @@ class VIC {
             0xB6591C, 0x683808, 0xEA746C, 0x4D4D4D, 0x848484, 0xA6FA9E, 0x707CE6, 0xB6B6B5
         )*/
         val COLOR_TABLE = arrayOf(
+            //black,  white,    red,      cyan,     purple,   green,    blue,     yellow
             0x000000, 0xFFFFFF, 0x924A40, 0x84C5CC, 0x9351B6, 0x72B14B, 0x483AAA, 0xD5DF7C,
+            //orange, brown,    pink,    dark grey, gray,    lt green, lt blue,   lt gray
             0x99692D, 0x675200, 0xC18178, 0x606060, 0x8A8A8A, 0xB3EC91, 0x867ADE, 0xB3B3B3
         )
     }
@@ -144,42 +146,84 @@ class VIC {
     }
 
     private fun rasterBitmapMode(rasterline: Int, y: Int) {
-        // todo: implement multicolor-mode
         val isMulticolorMode = memory.fetch(VIC_SCROLX).toInt() and 0b0001_0000 == 0b0001_0000
         val videoBankAddress = getVideoBankAddress()
         val borderColor = COLOR_TABLE[memory.fetch(VIC_EXTCOL).toInt() and 0b0000_1111]
         val textRow = y / 8
         val textRowAddr = textRow * 40
         val charY = y.rem(8)
-
         val bitmapColorRowAddress = videoBankAddress + getBitmapColorAddress() + textRowAddr
         val bitmapRowAddress = videoBankAddress + getBitmapAddress() + textRowAddr * 8 + charY
+        val colorRamRowAddress = COLOR_RAM + textRowAddr
+        val backgroundColor = COLOR_TABLE[memory.fetch(VIC_BGCOL1).toInt() and 0b0000_1111]
 
-        for (rastercolumn in 0 until PAL_RASTERCOLUMNS) {
-            var color: Int
-            if (rasterline < 51 || rasterline > 250 || rastercolumn < 24 || rastercolumn > 343) {
-                // outer border color
-                color = borderColor
-            } else {
-                val x = rastercolumn - 24
-                val textCol = x / 8
-                val pxBit =  x and 0b0000_0111
-                val colors = memory.fetch(bitmapColorRowAddress + textCol).toInt()
-                // 8x8 colorblock - hi-nibble: pixel-color, lo-nibble: background-color
-                val bgColor = COLOR_TABLE[colors and 0b0000_1111]
-                val pxColor = COLOR_TABLE[colors and 0b1111_0000 shr 4]
-
-                // get byte from bitmap
-                val bitmapByte = memory.fetch(bitmapRowAddress + textCol * 8).toInt()
-                // read the correct bit
-                val bitTest = (0b1000_0000u shr pxBit).toInt()
-                color = if (bitmapByte and bitTest == bitTest) {
-                    pxColor
+        if (!isMulticolorMode) {
+            // hires bitmap mode
+            for (rastercolumn in 0 until PAL_RASTERCOLUMNS) {
+                var color: Int
+                if (rasterline < 51 || rasterline > 250 || rastercolumn < 24 || rastercolumn > 343) {
+                    // outer border color
+                    color = borderColor
                 } else {
-                    bgColor
+                    val x = rastercolumn - 24
+                    val textCol = x / 8
+                    val pxBit = x and 0b0000_0111
+                    val colors = memory.fetch(bitmapColorRowAddress + textCol).toInt()
+                    // 8x8 colorblock - hi-nibble: pixel-color, lo-nibble: background-color
+                    val bgColor = COLOR_TABLE[colors and 0b0000_1111]
+                    val pxColor = COLOR_TABLE[colors and 0b1111_0000 shr 4]
+
+                    // get byte from bitmap
+                    val bitmapByte = memory.fetch(bitmapRowAddress + textCol * 8).toInt()
+                    // read the correct bit
+                    val bitTest = (0b1000_0000u shr pxBit).toInt()
+                    color = if (bitmapByte and bitTest == bitTest) {
+                        pxColor
+                    } else {
+                        bgColor
+                    }
                 }
+                bitmapData.setRGB(rastercolumn, rasterline, color)
             }
-            bitmapData.setRGB(rastercolumn, rasterline, color)
+        } else {
+            // todo - later merge code with code for hires...
+            // multicolor bitmap mode
+            for (rastercolumn in 0 until PAL_RASTERCOLUMNS) {
+                var color: Int
+                if (rasterline < 51 || rasterline > 250 || rastercolumn < 24 || rastercolumn > 343) {
+                    // outer border color
+                    color = borderColor
+                } else {
+                    val x = rastercolumn - 24
+                    val textCol = x / 8
+                    val pxBit = x and 0b0000_0110
+                    val colors = memory.fetch(bitmapColorRowAddress + textCol).toInt()
+                    // 8x8 colorblock - hi-nibble: hi-color, lo-nibble: lo-color
+                    val lo_color = COLOR_TABLE[colors and 0b0000_1111]
+                    val hi_color = COLOR_TABLE[colors and 0b1111_0000 shr 4]
+                    val color_ram = COLOR_TABLE[memory.fetch(colorRamRowAddress + textCol).toInt() and 0b0000_1111]
+
+                    // get byte from bitmap
+                    val bitmapByte = memory.fetch(bitmapRowAddress + textCol * 8).toInt()
+                    // read the correct bit
+                    val bitTest = (0b1100_0000u shr pxBit).toInt()
+                    color = when ((bitmapByte and bitTest) shr (6-pxBit)) {
+                        0b00 -> {
+                            backgroundColor
+                        }
+                        0b01 -> {
+                            hi_color
+                        }
+                        0b10 -> {
+                            lo_color
+                        }
+                        else -> {
+                            color_ram
+                        }
+                    }
+                }
+                bitmapData.setRGB(rastercolumn, rasterline, color)
+            }
         }
     }
 
